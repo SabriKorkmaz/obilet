3784e9b14e9024256c0ca3598929906ed59edf69
Sabri Korkmaz <sabri_51@hotmail.com>
2020-08-01 21:23:07 +0300

    init

--- src/routes/detail/index.js
+++ src/routes/detail/index.js
@@ -16,14 +16,14 @@ class Detail extends Component {
   }
 
   componentWillMount() {
-    let requestParam = {
-      sessionId: this.props.Session.sessionId,
-      deviceId: this.props.Session.deviceId,
-      originId: parseInt(this.props.Locations.origin.value),
-      destinationId: parseInt(this.props.Locations.destination.value),
-      date: this.props.Locations.date,
-    };
-    getJourneys(requestParam);
+    // let requestParam = {
+    //   sessionId: this.props.Session.sessionId,
+    //   deviceId: this.props.Session.deviceId,
+    //   originId: parseInt(this.props.Locations.origin.value),
+    //   destinationId: parseInt(this.props.Locations.destination.value),
+    //   date: this.props.Locations.date,
+    // };
+    // getJourneys(requestParam);
   }
   render() {
 
@@ -42,7 +42,7 @@ class Detail extends Component {
               {this.props.Locations.origin.label} -{" "}
               {this.props.Locations.destination.label}
             </h4>
-            <h6>{date}</h6>
+            {/* <h6>{date}</h6> */}
           </div>
           {this.props.Detail.journeyList.map(function(val, i) {
             return (
@@ -63,11 +63,6 @@ class Detail extends Component {
   }
 }
 
-const mapStateToProps = (state, ownProps) => ({
-  Detail: state.Detail,
-  Session: state.Session,
-  Locations: state.Locations,
-});
-Detail = connect(mapStateToProps)(Detail);
+
 
 export default Detail;
